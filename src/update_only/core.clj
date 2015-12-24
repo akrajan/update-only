7585910811a35683050d747609ef01651d0b5f6a
akrajan <arun.kumar.rajan.n@gmail.com>
2015-12-24 17:44:51 +0530

    Removing junk file

--- src/update_only/core.clj
+++
@@ -1,6 +0,0 @@
-(ns update-only.core
-  (:use [clojure.string :only (replace-first)])
-  (:require [cheshire.core :refer [parse-string]]
-            [clojure.reflect :refer [reflect]]
-            [clojure.set :as set]))
-
